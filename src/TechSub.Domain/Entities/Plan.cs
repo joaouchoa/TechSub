6559ba6da6639f9239ef3ba792cdf3a237e03cab
joaouchoa <joaodep2@gmail.com>
2026-03-05 17:13:15 -0300

    feature: Modelagem de Dominio inicial do Plano.

---
+++ src/TechSub.Domain/Entities/Plan.cs
@@ -0,0 +1,31 @@
+﻿namespace TechSub.Domain.Entities;
+
+public class Plan : BaseEntity
+{
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public decimal Price { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public Plan(string title, string description, decimal price)
+    {
+        Title = title;
+        Description = description;
+        Price = price;
+        IsActive = true;
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
+        UpdateTimestamp();
+    }
+
+    public void UpdateInfo(string title, string description, decimal price)
+    {
+        Title = title;
+        Description = description;
+        Price = price;
+        UpdateTimestamp();
+    }
+}
