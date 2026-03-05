212da8e439aa40105dc016278a24856107a423c6
joaouchoa <joaodep2@gmail.com>
2026-03-05 02:14:01 -0300

    feature: Modelagem inicial do Dominio

---
+++ src/TechSub.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TechSub.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime? UpdatedAt { get; protected set; }
+
+        protected BaseEntity()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateTimestamp()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/TechSub.Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSub.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; }
+
+        private User() { }
+
+        public User(string name, string email, string passwordHash)
+            : base() 
+        {
+            Name = name;
+            Email = email;
+            PasswordHash = passwordHash;
+        }
+
+        public void UpdatePassword(string newPasswordHash)
+        {
+            PasswordHash = newPasswordHash;
+            UpdateTimestamp();
+        }
+    }
+}
